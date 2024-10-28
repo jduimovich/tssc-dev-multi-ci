dd83944469862d71848417293c8be12091bb5d47
gitops-update <rhtap@noreplay.com>
2024-10-28 12:38:52 -0400

    update test ci

--- ci-test.sh
+++ ci-test.sh
@@ -7,39 +7,41 @@ if [ $TEST_REPO_ORG == "redhat-appstudio" ]; then
     echo "You must create forks in your own org and set up MY_TEST_REPO_ORG (github) and MY_TEST_REPO_GITLAB_ORG"
     exit
 fi
-
-# The dev repos should can be enabled for Jenkins, gitlab and github actions
-# to enable gitlab, add a remote in the github repo to match the repo in gitlab
-
-OPTIONAL_REPO_UPDATE= 
-
+ 
 #Jenkins 
-
 echo "Update Jenkins file in $BUILD and $GITOPS" 
-
+echo "Jenkins to reuse the Github repo" 
 cp Jenkinsfile $BUILD/Jenkinsfile   
 cp Jenkinsfile.gitops $GITOPS/Jenkinsfile
 # ENV with params
 
-mkdir -p $BUILD/rhtap
-SETUP_ENV=$BUILD/rhtap/env.sh
-cp rhtap/env.template.sh $SETUP_ENV
-sed -i "s!\${{ values.image }}!quay.io/\${MY_QUAY_USER:-jduimovich0}/bootstrap!g" $SETUP_ENV
-sed -i "s!\${{ values.dockerfile }}!Dockerfile!g" $SETUP_ENV
-sed -i "s!\${{ values.buildContext }}!.!g" $SETUP_ENV
-sed -i "s!\${{ values.repoURL }}!$OPTIONAL_REPO_UPDATE!g" $SETUP_ENV
-
-# Set MY_REKOR_HOST and MY_TUF_MIRROR to 'none' if these services are not available
-sed -i 's!export REKOR_HOST=.*$!export REKOR_HOST="\${MY_REKOR_HOST:-http://rekor-server.rhtap.svc}"!' $SETUP_ENV
-sed -i 's!export TUF_MIRROR=.*$!export TUF_MIRROR="\${MY_TUF_MIRROR:-http://tuf.rhtap.svc}"!' $SETUP_ENV
-
-echo "# Update forced CI test $(date)" >>$SETUP_ENV
-cat $SETUP_ENV
+function updateBuild() { 
+    REPO=$1
+    GITOPS_REPO_UPDATE=$2
+    mkdir -p $REPO/rhtap
+    SETUP_ENV=$REPO/rhtap/env.sh
+    cp rhtap/env.template.sh $SETUP_ENV
+    sed -i "s!\${{ values.image }}!quay.io/\${MY_QUAY_USER:-jduimovich0}/bootstrap!g" $SETUP_ENV
+    sed -i "s!\${{ values.dockerfile }}!Dockerfile!g" $SETUP_ENV
+    sed -i "s!\${{ values.buildContext }}!.!g" $SETUP_ENV
+    sed -i "s!\${{ values.repoURL }}!$GITOPS_REPO_UPDATE!g" $SETUP_ENV
+    # Set MY_REKOR_HOST and MY_TUF_MIRROR to 'none' if these services are not available
+    sed -i 's!export REKOR_HOST=.*$!export REKOR_HOST="\${MY_REKOR_HOST:-http://rekor-server.rhtap.svc}"!' $SETUP_ENV
+    sed -i 's!export TUF_MIRROR=.*$!export TUF_MIRROR="\${MY_TUF_MIRROR:-http://tuf.rhtap.svc}"!' $SETUP_ENV
+    echo "# Update forced CI test $(date)" >>$SETUP_ENV
+    cat $SETUP_ENV
+}
+# Repos on github and gitlab, github reused for Jenkins
+# source repos get the name of the corresponding GITOPS REPO
+updateBuild $BUILD $TEST_GITOPS_GITLAB_REPO
+updateBuild $GITOPS  
+updateBuild $GITLAB_BUILD $TEST_GITOPS_GITLAB_REPO
+updateBuild $GITLAB_GITOPS 
 
 # Gitlab CI  
 echo "Update .gitlab-ci.yml file in $BUILD and $GITOPS" 
-cp .gitlab-ci.yml $BUILD/.gitlab-ci.yml
-cp .gitlab-ci.gitops.yml $GITOPS/.gitlab-ci.yml
+cp .gitlab-ci.yml $GITLAB_BUILD/.gitlab-ci.yml
+cp .gitlab-ci.gitops.yml $GITLAB_GITOPS/.gitlab-ci.yml
 
 # Github Actions  
 echo "Update .github workflows in $BUILD and $GITOPS"  
@@ -58,28 +60,23 @@ function updateRepos() {
     echo 
     echo "Updating $REPO" 
     pushd $REPO
-    git add .
-    git commit -m "Testing in CI"
-    git push 
-    echo "Testing for gitlab remote, push if success $REPO" 
-    # echo need a remote named gitlab that is based on this repo
-    git remote | grep "gitlab"
-    ERR=$? 
-    if [ $ERR == 0 ]; then
-        git push gitlab main 
-    else 
-        echo "no remote in $REPO for gitlab"
-    fi 
+    git add . 
+    git commit -m "Testing in CI"  
+    git push   
     popd
 }
 
-updateRepos $BUILD     # does github and gitlab build repos with vars
-bash hack/ghub-set-vars $TEST_BUILD_REPO
-bash hack/glab-set-vars $(basename $TEST_BUILD_GITLAB_REPO) 
+# github 
+updateRepos $BUILD      
+updateRepos $GITOPS  
+# gitlab
+updateRepos $GITLAB_BUILD    
+updateRepos $GITLAB_GITOPS   
 
-updateRepos $GITOPS # does github and gitlab 
-bash hack/ghub-set-vars $TEST_GITOPS_REPO
-bash hack/glab-set-vars $(basename $TEST_GITOPS_GITLAB_REPO) 
+bash hack/ghub-set-vars $TEST_BUILD_REPO 
+bash hack/ghub-set-vars $TEST_GITOPS_REPO  
+bash hack/glab-set-vars $(basename $TEST_BUILD_GITLAB_REPO) 
+bash hack/glab-set-vars $(basename $TEST_GITOPS_GITLAB_REPO)   
 
 echo "Github Build and Gitops Repos" 
 echo "Build: $TEST_BUILD_REPO"
